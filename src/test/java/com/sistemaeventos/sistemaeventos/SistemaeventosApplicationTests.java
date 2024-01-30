c55f6e54ccb8a3684f458e8d07a600f48e77f0a6
Rafael Neves <rafael.nsouza@goias.gov.br>
2024-01-30 13:09:31 -0300

    First

---
+++ src/test/java/com/sistemaeventos/sistemaeventos/SistemaeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaeventos.sistemaeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
